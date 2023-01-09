152991644bbf9e42771570f5d2bbcf0aabea26eb
bakachan <kienn11000@gmail.com>
2023-01-10 02:31:35 +0700

    update_table_warehouse

--- fruit-ai/src/pages/Warehouse/Warehouse.tsx
+++ fruit-ai/src/pages/Warehouse/Warehouse.tsx
@@ -1,16 +1,20 @@
 import Icon, { DeleteOutlined, EditOutlined, LoginOutlined } from '@ant-design/icons';
-import { Button, Table } from "antd";
-import type { ColumnsType } from 'antd/es/table';
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { Button } from "antd";
 import Title from "antd/es/typography/Title";
 import { useEffect, useState } from "react";
-import './warehouse.scss';
+import { IWareHouse } from '../../common/define-fruit';
+import DetailsIcon from '../../images/default_icon.png';
+import { setWareHouse } from '../../redux/controller';
 import { useDispatchRoot, useSelectorRoot } from '../../redux/store';
-import { getAllWarehouseByUserIdRequest, setWareHouse } from '../../redux/controller';
-import { IWareHouse, WarehouseRequest } from '../../common/define-fruit';
-import Column from 'antd/es/table/Column';
-import DetailsIcon from '../../images/default_icon.png'
 import AddWarehouseModel from './AddWarehouseModel';
-
+import './warehouse.scss';
 // interface MyProps{
 //     lstWarehouse: IWareHouse[]
 // }
@@ -21,50 +25,12 @@ interface DataType {
     name: string;
     address: string;
 }
-const columns = [
-    {
-        title: 'STT',
-        dataIndex: 'orderByNumber',
-    },
-    {
-        title: 'Tên kho',
-        dataIndex: 'name',
-    },
-    {
-        title: 'Địa chỉ',
-        dataIndex: 'address',
-    },
-    {
-        title: "Mặc định",
-        key: "default",
-        render: () => (
-            <div>
-                <Button className='default-button'>
-                    <Icon className='default-button-icon' component={() => (<img className='icon-default' src={DetailsIcon} />)} />
-                    <span className='default-button-text'>Đặt làm mặt định</span>
-                </Button>
-            </div>
-        )
-    },
-    {
-        title: "Thao tác",
-        key: "action",
-        render: () => (
-            <div>
-                <EditOutlined className='action' />
-                <LoginOutlined className='action' />
-                <DeleteOutlined className='action' />
-            </div>
-        )
-    }
-];
 
 const Warehouse = () => {
     const [lstWareHouse, setLstWareHouse] = useState<DataType[]>([])
-    const [isOpenModal,setIsOpenModal] = useState<boolean>(false);
+    const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
     const { sliceLstWarehouses } = useSelectorRoot(item => item.fruit);
     const dispatch = useDispatchRoot();
-
 
     useEffect(() => {
         const warehouses = []
@@ -80,25 +46,22 @@ const Warehouse = () => {
         setLstWareHouse(warehouses);
     }, [sliceLstWarehouses])
 
-    // rowSelection object indicates the need for row selection
-    const rowSelection = {
-        onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
-            const warehouseSelect: IWareHouse = {
-                id: selectedRows[0].id,
-                name: selectedRows[0].name,
-                address: selectedRows[0].address,
-            }
-            dispatch(setWareHouse(warehouseSelect))
-            console.log('selectedRows: ', warehouseSelect);
-            window.location.reload();
-        },
-    };
+    const rowSelection = (selectedRows: any) => {
+        const warehouseSelect: IWareHouse = {
+            id: selectedRows.id,
+            name: selectedRows.name,
+            address: selectedRows.address,
+        }
+        dispatch(setWareHouse(warehouseSelect))
+        console.log('selectedRows: ', warehouseSelect);
+        window.location.reload();
+    }
 
     const toggleAddWarehouseModal = () => {
         setIsOpenModal(!isOpenModal);
     }
+
     return (
-        
         <div className="warehouse-main">
             <AddWarehouseModel
                 isOpenModal={isOpenModal}
@@ -106,19 +69,54 @@ const Warehouse = () => {
             />
             <div className="warehouse-title">
                 <Title className="title-text" level={3}>Vui lòng chọn kho đặt làm mặc định!</Title>
-                <Button 
+                <Button
                     className="title-button"
-                    onClick={() => setIsOpenModal(true)}
-                >
+                    onClick={() => setIsOpenModal(true)}>
                     Tạo kho mới
                 </Button>
             </div>
             <div className="warehouse-table">
-                <Table
-                    rowSelection={rowSelection}
-                    columns={columns}
-                    dataSource={lstWareHouse} >
-                </Table>
+                <TableContainer component={Paper}>
+                    <Table size="small" aria-label="a dense table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">STT</TableCell>
+                                <TableCell align="center">Tên kho</TableCell>
+                                <TableCell align="center">Địa chỉ</TableCell>
+                                <TableCell align="center">Mặc định</TableCell>
+                                <TableCell align="center">Thao tác</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {lstWareHouse.map((warehouse) => (
+                                <TableRow
+                                    onClick={() => rowSelection(warehouse)}
+                                    key={warehouse.name}
+                                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                                >
+                                    <TableCell align="center" component="th" scope="row">
+                                        {warehouse.orderByNumber}
+                                    </TableCell>
+                                    <TableCell align="center">{warehouse.name}</TableCell>
+                                    <TableCell align="center">{warehouse.address}</TableCell>
+                                    <TableCell align="center">
+                                        <Button className='default-button'>
+                                            <Icon className='default-button-icon' component={() => (<img className='icon-default' src={DetailsIcon} />)} />
+                                            <span className='default-button-text'>Đặt làm mặt định</span>
+                                        </Button>
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        <div>
+                                            <EditOutlined className='action' />
+                                            <LoginOutlined className='action' />
+                                            <DeleteOutlined className='action' />
+                                        </div>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
         </div>
     )
